b07097090e12a3794eb94f3b5576006b8872bc2f
Viennarz Curtiz <viennarzc@gmail.com>
2020-07-08 19:23:58 +0800

    Initial Commit

---
+++ PHDivisions/PHDivisions.h
@@ -0,0 +1,19 @@
+//
+//  PHDivisions.h
+//  PHDivisions
+//
+//  Created by SCI-Viennarz on 7/8/20.
+//  Copyright © 2020 VVC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PHDivisions.
+FOUNDATION_EXPORT double PHDivisionsVersionNumber;
+
+//! Project version string for PHDivisions.
+FOUNDATION_EXPORT const unsigned char PHDivisionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHDivisions/PublicHeader.h>
+
+
